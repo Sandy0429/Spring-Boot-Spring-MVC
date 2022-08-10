cd3e8f2d46f5018cad9b36046d13739f99254ca7
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-10 10:36:51 +0530

    Create InternationalizationApplication.java

---
+++ Front End-Internationalization/InternationalizationApplication.java
@@ -0,0 +1,17 @@
+//Internationalization/src/main/java/com/example/demo/InternationalizationApplication.java
+    package com.example.demo;
+    
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.context.annotation.ComponentScan;
+    
+    @SpringBootApplication
+    @ComponentScan({ "controller", "model" })    
+    public class InternationalizationApplication {
+    
+    	public static void main(String[] args) {
+    		SpringApplication.run(InternationalizationApplication.class, args);
+    	}
+    
+    }
+   
